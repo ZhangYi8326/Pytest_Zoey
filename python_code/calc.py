539828e50b98cb6044fb8c67e0340a6555150835
Zoey <891758726@qq.com>
2021-02-02 00:20:07 +0800

    计算器

---
+++ python_code/calc.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/2/1 21:11
+@Auth ： Zoey
+@File ：calc.py
+@Description：
+"""
+
+
+# 计算器
+class Calculator:
+    # 加法
+    def add(self, a, b):
+        return a+b
+
+    # 减法
+    def sub(self, a, b):
+        return a-b
+
+    # 乘法
+    def mul(self, a, b):
+        return a*b
+
+    # 除法
+    def div(self, a, b):
+        return a/b
